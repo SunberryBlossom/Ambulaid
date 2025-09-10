bb7533960561efad1fc1613b9c1d365bb0c6ac75
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-09-10 10:15:04 +0200

    Added comments, removed initial comment regarding goals of the program.

--- Program.cs
+++ Program.cs
@@ -4,19 +4,6 @@
     {
         static void Main(string[] args)
         {
-            /*
-                Goals of program:           
-                     When all answers are collected, decide if red, yellow or green
-                     Red IF: diff breathing OR chest pain | major bleeding | temp above 40c AND age <1 or >75
-                     Yellow IF: not red, but: temp >=38.5 | pain >= 7
-                     Green IF: not red or yellow
-
-                    Print out to the console:
-                    - Name of patient
-                    - Choosen priority
-                    - Short motivation, i.e. YELLOW: fever 38.7 and pain level 7
-            */
-
             // Greets user, asks for compliance for the following data inputs
             Console.WriteLine("\n\t\tHello user! Welcome to Ambulaid.");
             Console.WriteLine("\t\tPlease enter the following. . .");
@@ -27,14 +14,14 @@
             string patientName = Console.ReadLine();
 
             Console.Write("\t\tAge of patient (enter in numerals, i.e. 12): ");
-            double patientAge = double.Parse(Console.ReadLine());
+            float patientAge = float.Parse(Console.ReadLine());
 
             Console.Write("\t\tCurrent temperature of patient (enter in celcius): ");
-            double patientTemp = double.Parse(Console.ReadLine());
+            float patientTemp = float.Parse(Console.ReadLine());
             Console.Clear();
 
 
-            // Ask for current symptoms. Save answers in strings.
+            // Ask for current symptoms. Save answers in strings except pain level. Need it to be numeral for later if-statements
             Console.Write("\n\t\tIs the patient currently suffering from chest pains? [y/n]: ");
             string chestPain = Console.ReadLine();
             Console.Clear();
@@ -56,6 +43,7 @@
             string alarmMessage = "";
 
 
+            // If statement to categorize alarm color based on reported symptoms
             if ((chestPain == "y" || breathingDiff == "y") || majorBleeding == "y" || patientTemp > 40 && (patientAge < 1 || patientAge > 75))
             {
                 alarmColor = "RED";
@@ -69,6 +57,7 @@
                 alarmColor = "GREEN";
             }
 
+            // alarm message concatenation of positive symptoms.
             if (chestPain == "y") alarmMessage += "chestpains. ";
             if (breathingDiff == "y") alarmMessage += "Difficulty breathing. ";
             if (majorBleeding == "y") alarmMessage += "A major bleeding. ";
@@ -77,6 +66,7 @@
             if (alarmMessage == "") alarmMessage = "No major symptoms.";
 
 
+            // Prints out patient name, alarm status and motivation for picking that status.
             Console.WriteLine($"\n\t\tPatient name: {patientName}");
             Console.WriteLine($"\t\tPriority: {alarmColor}");
             Console.WriteLine($"\t\tMotivation: {alarmMessage}");
